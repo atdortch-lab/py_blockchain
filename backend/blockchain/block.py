cb66e543db22e30979788b3ad2c376668c72a3b7
atdortch <atdortch@gmail.com>
2025-04-07 20:17:41 -0400

    added tests for mine rate and difficulty.

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -1,5 +1,7 @@
 import time
 from backend.util.crypto_hash import crypto_hash
+from backend.util.hex_to_binary import hex_to_binary
+from backend.config import MINE_RATE
 
 GENESIS_DATA = {
     'timestamp': 1,
@@ -23,6 +25,9 @@ class Block:
         self.difficulty = difficulty
         self.nonce = nonce
 
+    def add_block(self, data):
+        self.chain.append(Block(data))
+
     def __repr__(self):
         return (
             'Block('
@@ -42,13 +47,14 @@ class Block:
         """""
         timestamp = time.time_ns()
         last_hash = last_block.hash
-        difficulty = last_block.difficulty
+        difficulty = Block.adjust_difficulty(last_block, timestamp)
         nonce = 0
         hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
-        while hash[0:difficulty] != '0' * difficulty:
+        while hex_to_binary(hash)[0:difficulty] != '0' * difficulty:
             nonce += 1
             timestamp = time.time_ns()
+            difficulty = Block.adjust_difficulty(last_block, timestamp)
             hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
         return Block(timestamp, last_hash, hash, data, difficulty, nonce)
@@ -67,6 +73,20 @@ class Block:
         )
         """""
         return Block(**GENESIS_DATA)
+    
+    @staticmethod
+    def adjust_difficulty(last_block, new_timestamp):
+        """
+        Calculate the adjusted difficulty according to the MINE_RATE.
+        Increase the difficulty for quickly mined blocks.
+        Decrease the difficulty for slowly mined block.
+        """
+        if (new_timestamp - last_block.timestamp) < MINE_RATE:
+            return last_block.difficulty + 1
+        if(last_block.difficulty - 1) > 0:
+            return last_block.difficulty - 1
+        
+        return 1
     
 def main():
     genesis_block = Block.genesis()
